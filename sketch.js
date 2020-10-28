4f8a8999b1fab89bcecd615d8de46fe261c19f31
nellysaad11 <64480661+nellysaad11@users.noreply.github.com>
2020-10-27 23:57:02 -0400

    2020-03-nellysaad11

--- sketch.js
+++ sketch.js
@@ -23,7 +23,7 @@ background (bg);
 function draw(){
   //runs continuously
 
-text("click locations you have visited", 0, 0, width);
+text("click places you have visited", 0, 0, width);
 fill("#db3236	");
 textSize(20);
 textFont("AVENIR");
@@ -40,55 +40,3 @@ function mouseClicked(){
   savedx=mouseX;
   sound.play();
 }
-
-
-
-
-
-
-/*
-let bg;
-let song = [];
-let pins;
-
-function preload(){
-soundFormats('mp3');
- song = loadSound("./assets/sound1.mp3");
-
-  // put preload code here
-}
-
-function setup() {
-  createCanvas(windowWidth,windowHeight);
-  bg= loadImage("./assets/world-map.jpg");
-  pins=loadImage("./assets/pins.png");
-  song=loadSound("./assets/sound1.mp3");
-
-     }
-
-function draw() {
-  background (bg);
-//text
-let textA= "Click Your Location"
-  fill("#E48582");
-  textSize(60);
-  textAlign(CENTER, BASELINE);
-  text(textA, 0, 0, width);
-  textFont("Avenir");
-  textStyle(BOLD);
-
-class canvas{
-  constructor(x,y){
-  this.x (windowWidth);
-  this.y (windowHeight);
-
-}
-}
-function mouseClicked() {
-    loadImage.pins;
-      Song.play();
-    }
-
-
-  }
-  */
